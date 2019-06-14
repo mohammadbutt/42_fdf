dd4843bff320abc274c29f240a5cb4f9a2cf6934
Mohammad Butt <mbutt@student.42.us.org>
2019-06-14 15:01:50 -0700

    Updating files

--- test_draw_old.c
+++ test_draw_old.c
@@ -6,60 +6,63 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 22:20:11 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/14 15:01:21 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/06/13 20:52:18 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-
-static void random_color(void *param)
+/*
+int main(void)
 {
-	int red;
-	int green;
-	int blue;
+	ft_putstr("This is a test\n");
+}
+*/
+int color_with_c(int key, void *param)
+{
+	static int i;
 
-	red = (rand() % 127);
-	green = (rand() % 127);
-	blue = (rand() % 127);
+	i = 5;
+	if(key == 8)
+		return(i);
+	return(0);
 }
 
-int deal_keys(int key, void *param)
+int deal_key(int key, void *param)
 {
 	if(key == 0)
 		ft_putchar('A');
+	if(key == 1)
+		ft_putchar('S');
+	if(key == 2)
+		ft_putchar('D');
+	if(key == 3)
+		ft_putchar('F');
 	if(key == 8)
-		random_color(param);
+	{
+		color_with_c(key, param);
+		return(8);
+	}
 	if(key == 53)
 		exit(0);
+//	if(key == 8)
+//		mlx_get_color_value(mlx_ptr, 0x696969);
 	return(0);
 }
 
-int draw_function(void *param)
-{
-	void *mlx_ptr;
-	void *win_ptr;
-	void *img_ptr;
 
-	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "This is mlx 42 - by Mo");
-	mlx_string_put(mlx_ptr, win_ptr, 250, 250, 127, "(_______)" );
+int deal_button(int key, void *param)
+{
+	if(key == 1)
+		ft_putchar('1');
+	if(key == 2)
+		ft_putchar('2');
+	if(key == 4)
+		ft_putchar('4');
+	if(key == 5)
+		ft_putchar('5');
 	return(0);
 }
 
-int main(void)
-{
-	void *mlx_ptr;
-	void *win_ptr;
-	void *img_ptr;
-
-	mlx_ptr = mlx_init();
-	mlx_hook(win_ptr, 2, 5, deal_keys, (void *)0);
-	mlx_loop_hook(mlx_ptr, draw_function, (void *)0);
-	
-	mlx_loop(mlx_ptr);
-}
-
-/*
 int main(void)
 {
 	void *mlx_ptr;
@@ -96,16 +99,16 @@ int main(void)
 //	mlx_string_put(mlx_ptr, win_ptr, 250, 430, 0xff0000, "    |_|  ");
 
 //	mlx_key_hook(win_ptr, deal_key, (void *)0);
-//	mlx_hook(win_ptr, 2, 5, deal_key, (void *)0); //Allows long key presses. Works
+//	mlx_hook(win_ptr, 2, 5, deal_key, (void *)0); /*Allows long key presses. Works*/
 //	mlx_hook(win_ptr, 3, key_release, (int) x_mask, (void *)0);
 	mlx_mouse_hook(win_ptr, deal_button, (void *)0);
 	mlx_hook(win_ptr, 2, 5, deal_key, (void *)0);
 //	mlx_hook(win_ptr, 2, 5, color_with_c, (void *)0);
 //	mlx_string_put(mlx_ptr, win_ptr, 250, 430, range[deal_key], "    |_|  ");
 
-	img_ptr = mlx_new_image(mlx_ptr, 150, 150); //Image manipulation
+	img_ptr = mlx_new_image(mlx_ptr, 150, 150); /*Image manipulation*/
 	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 130, 130);
 
-	mlx_loop(mlx_ptr); //Tells the system that we want to print something
+	mlx_loop(mlx_ptr); /*Tells the system that we want to print something*/
 }
-*/
+
