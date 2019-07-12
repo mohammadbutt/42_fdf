12d4592ae004a642a168f8885d9136d5bacc3c18
Mohammad Butt <mbutt@student.42.us.org>
2019-07-12 11:08:26 -0700

    Drawing perfect squares

--- square_test.c
+++ square_test.c
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 20:27:24 by mbutt             #+#    #+#             */
-/*   Updated: 2019/07/11 22:06:52 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/07/12 11:08:05 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,26 @@ typedef struct 	s_mlx1
 */
 void draw_square(t_mlx **mlx, int x, int y, int size)
 {
-	int temp_size;
+	int size_x;
+	int size_y;
+	int temp_x;
+	int temp_y;
 
-//	x = x + size;
-//	y = y + size;
-
-	temp_size = y;
-	size = size + x;
-	while(x < size)
+	size_x = size + x;
+	size_y = size + y;
+	temp_x = x;
+	temp_y = y;
+	while(x < size_x)
 		mlx_pixel_put((*mlx)->mlx_ptr, (*mlx)->win_ptr, x++, y, 0xff0000);
-	while(y < size)
+	while(y < size_y)
 		mlx_pixel_put((*mlx)->mlx_ptr, (*mlx)->win_ptr, x, y++, 0xff0000);
-	while(x > temp_size)
+	while(x > temp_x)
 		mlx_pixel_put((*mlx)->mlx_ptr, (*mlx)->win_ptr, x--, y, 0xff0000);
-	while(y > temp_size)
+	while(y > temp_y)
 		mlx_pixel_put((*mlx)->mlx_ptr, (*mlx)->win_ptr, x, y--, 0xff0000);
 }
 
-void draw_one_line(t_mlx **mlx, int x, int y, int size)
+void draw_horizontal_line(t_mlx **mlx, int x, int y, int size)
 {
 	size = size + x;
 	while(x < size)
@@ -49,32 +51,17 @@ int main(void)
 	t_mlx *mlx;
 	int x;
 	int y;
-	int z;
-//	void *mlx_ptr;
-//	void *win_ptr;
+	int a;
 
-	x = 100;
-	y = 100;
-	z = 100;
+	x = 20;
+	y = 15;
+//	a = 100;
 	mlx = malloc(sizeof(t_mlx));
 	mlx->mlx_ptr = mlx_init();
 	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, 1280, 720, "test screen");
 	
-	draw_square(&mlx, x, y, 200);
-	draw_one_line(&mlx, x, y, 200);	
-	while(z < 200)
-		mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, z++, 45, 0xffffff);
-//	mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xff0000);
-/*
-	while(x < 200)
-		mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, x++, 50, 0xff0000);
-	while(y < 200)
-		mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, x, y++, 0xff0000);
-	while(x > 50)
-		mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, x--, y, 0xff0000);
-	while(y > 50)
-		mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, x, y--, 0xff0000);
-*/
+	draw_square(&mlx, x, y, 50);
+	draw_horizontal_line(&mlx, x, 10, 50);
 
 	mlx_loop(mlx->mlx_ptr);
 }
