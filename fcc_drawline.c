5e06bb64dae96779b09027589fa097d594a7d721
Mohammad Butt <mbutt@student.42.us.org>
2019-07-17 20:23:58 -0700

    Draws a flat map. Need to break it into functions=

--- fcc_drawline.c
+++ fcc_drawline.c
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 18:01:00 by mbutt             #+#    #+#             */
-/*   Updated: 2019/07/17 19:24:02 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/07/17 20:23:00 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -253,49 +253,15 @@ void ft_render(t_mlx *mlx)
 	{
 		while(temp.map_width)
 		{
-/*			
-			mlx->x1 = mlx->x1 - 15; //To plot vertical line
-			mlx->y0 = mlx->y0 - 15; //To plot vertical line
-			printf("V: x0:|%d|, x1:|%d|\n y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
-			plot_any_line(mlx); //Plot Vertical linei
-			mlx->x1 = mlx->x1 + 15; //To plot horizontal line
-//			mlx->y0 = mlx->y0 - 15; //To plt horizontal line
-//			mlx->y1 = mlx->y1 - 15;
-			printf("H:x0:|%d|, x1:|%d|\n y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
-			plot_any_line(mlx); //Plot Horizontal line
-			printf("x0:|%d|, x1:|%d|\n y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
-//			ft_exit("Exiting\n");
-			mlx->x0 = mlx->x1;
-			mlx->x1 = mlx->x1 + 15;
-			printf("x0:|%d|, x1:|%d|\n y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
-			temp.map_width--;
-*/
-/*
-// Works, Vertical first then Horizontal
-			mlx->x0 = mlx->x0 + 15;
-			mlx->y0 = mlx->y0 - 15;
-			printf("V: x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
-			plot_any_line(mlx);
-			mlx->x0 = mlx->x0 - 15;
-			printf("H: x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
-			plot_any_line(mlx);
-			printf("   x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
-//			ft_exit("Exiting\n");
-			mlx->x0 = mlx->x1;
-			mlx->x1 = mlx->x1 + 15;
-			temp.map_width--;
-*/
 //Horizontal first, then vertical
 			printf("H: x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
 			plot_any_line(mlx);
 			mlx->x0 = mlx->x0 - 15;
 			mlx->x1 = mlx->x1 - 15;
-//			mlx->y1 = mlx->y1 - 15; // commenting for a quick test
 			mlx->y1 = mlx->y1 + 15; // Adding for test 0.1
 			printf("V: x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
 			plot_any_line(mlx);
 			printf("   x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
-	
 			mlx->y1 = mlx->y1 - 15;
 			mlx->y0 = mlx->y0 - 15;
 			mlx->x0 = mlx->x0 + 15;
@@ -305,10 +271,25 @@ void ft_render(t_mlx *mlx)
 		}
 		temp.map_width = mlx->map_width;
 		temp.map_height--;
+		printf("x0: x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
+		mlx->y0 = mlx->y0 + 15;
+		mlx->y1 = mlx->y1 + 15;	
+		printf("y0: x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
+
+		if(temp.map_height == 0)
+		{
+			printf("mlx->x0, mlx->y0, mlx->x1, mlx->y1:\n");
+			printf("x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
+			printf("temp.x0, temp.y0, temp.x1, temp.y1:\n");
+			printf("x0:|%d|, x1:|%d|\n   y0:|%d|, y1:|%d|\n\n", temp.x0, temp.x1, temp.y0, temp.y1);
+			mlx->x0 = temp.x0;
+			mlx->x1 = mlx->x1 - 15;
+			plot_any_line(mlx);
+			mlx->y0 = temp.y0;
+			plot_any_line(mlx);
+		}
 		mlx->x0 = temp.x0;
 		mlx->x1 = temp.x1;
-		mlx->y0 = mlx->y0 + 15;
-		mlx->y1 = mlx->y1 + 15;
 	}
 
 //	printf("x0:|%d|, x1:|%d|\n y0:|%d|, y1:|%d|\n\n", mlx->x0, mlx->x1, mlx->y0, mlx->y1);
