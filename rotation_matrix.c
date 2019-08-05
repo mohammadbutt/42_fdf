d9d6822f811d54147ce4fc620f0e9796e63d6eb0
Mohammad Butt <mbutt@student.42.us.org>
2019-08-05 11:01:54 -0700

    removed some more comments and extra functions2.

--- rotation_matrix.c
+++ rotation_matrix.c
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/25 12:55:42 by mbutt             #+#    #+#             */
-/*   Updated: 2019/08/04 20:22:22 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/08/05 11:00:18 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,6 @@ int		ft_min(t_mlx *mlx)
 		return(height);
 	else if(width <= height)
 		return(width);
-/*
-	if(height > width)
-		return(height);
-	else if(width >= height)
-		return(width);
-*/
 	return(0);
 }
 
@@ -59,114 +53,6 @@ void rotation_matrix(t_mlx *mlx, int *x, int *y, int z)
 	*x = (temp_x - temp_y) * cos(radian);
 	*y = -z + (temp_x + temp_y) * sin(radian);
 
-
-/*	
-//	if(mlx->camera == 0)
-//		mlx->degree_angle = 30;
-//	else if(mlx->camera == 1)
-//		mlx->degree_angle = 0;
-
-	radian = degree_to_radian(mlx->degree_angle);
-	temp_x = (*x * cos(radian)) - (*y * sin(radian));
-	temp_y = -z + (*x * sin(radian)) + (*y * cos(radian));
-
-	*x = temp_x;
-	*y = temp_y;
-*/
-}
-
-
-/*
-void rotation_matrix(t_mlx *mlx, int *x, int *y, int z)
-{
-	t_mlx temp;
-
-	double radian;
-
-	radian = degree_to_radian(mlx->degree_angle);
-//	temp.x = (x * cos(radian)) - (y * sin(radian));
-//	temp.y = -z + (x * sin(radian)) + (y * cos(radian));
-
-	temp.x = (*x + *y) * cos(radian);
-	temp.y = -z + (*x - *y) * sin(radian);
-
-	*x = temp.x;
-	*y = temp.y;
-}
-*/
-/*
-// Somewhat works, does not seg fault, but need to modify value that is
-// associated with each z0 and z1 so creating a new function above.
-void	rotation_matrix(t_mlx *mlx, t_mlx *temp, double degree_angle)
-{
-	t_mlx temp2;
-	double radian;
-
-	radian = degree_to_radian(degree_angle);
-//	temp2.x = (mlx->x1 * cos(radian)) - (mlx->y1 * sin(radian));
-//	temp2.y = (mlx->x1 * sin(radian)) + (mlx->y1 * cos(radian));
-	
-	temp2.x = (mlx->x1 + mlx->y1) * cos(radian);
-	temp2.y = ((mlx->x1 - mlx->y1) * sin(radian)); // Adding z value
-	temp2.y = -mlx->z1 + (mlx->x1 - mlx->y1) * sin(radian);
-
-	mlx->x1 = temp2.x;
-	mlx->y1 = temp2.y;
-}
-*/
-/*
-// Works and changed variables Need to implement z values.
-//void rotation_matrix(t_mlx *mlx, int *x, int *y, double degree_angle)
-void	rotation_matrix(t_mlx *mlx, t_mlx *temp, double degree_angle)
-{
-	t_mlx temp2;
-	double radian;
-
-	radian = degree_to_radian(degree_angle);
-	temp2.x = (mlx->x1 * cos(radian)) - (mlx->y1 * sin(radian));
-	temp2.y = (mlx->x1 * sin(radian)) + (mlx->y1 * cos(radian));
-
-	mlx->x1 = temp2.x;
-	mlx->y1 = temp2.y;
-}
-*/
-/*
-// Works, but changing variables
-void rotation_matrix(t_mlx *mlx, int *x, int *y, double degree_angle)
-{
-	t_mlx temp;
-	double radian;
-
-	radian = degree_to_radian(degree_angle);
-	temp.x = (*x * cos(radian)) - (*y * sin(radian));
-	temp.y = (*x * sin(radian)) + (*y * cos(radian));
-
-	*x = temp.x;
-	*y = temp.y;
-}
-*/
-void subtract_x0y0_from_x1y1(t_mlx *mlx, t_mlx *temp)
-{
-//	mlx->y1 = mlx->int_data[temp->y][temp->x+1];
-//	mlx->y0 = mlx->int_data[temp->y][temp->x];
-	mlx->x1 = mlx->x1 - mlx->x0;
-	mlx->y1 = mlx->y1 - mlx->y0;
-//	mlx->z1 = mlx->z1 - mlx->z0;
-}
-
-/*
-// Works, trying to add z values
-void subtract_x0y0_from_x1y1(t_mlx *mlx, t_mlx *temp)
-{
-	mlx->x1 = mlx->x1 - mlx->x0;
-	mlx->y1 = mlx->y1 - mlx->y0;
-}
-*/
-
-void add_rotated_x1y1_to_x0y0(t_mlx *mlx)
-{
-	mlx->x1 = mlx->x1 + mlx->x0;
-	mlx->y1 = mlx->y1 + mlx->y0;
 }
 
 void	copy_mlx_x0y0x1y1_to_temp_x0y0x1y1(t_mlx *mlx, t_mlx *temp)
@@ -175,14 +61,6 @@ void	copy_mlx_x0y0x1y1_to_temp_x0y0x1y1(t_mlx *mlx, t_mlx *temp)
 	temp->x1 = mlx->x1;
 	temp->y0 = mlx->y0;
 	temp->y1 = mlx->y1;
-}
-
-void	copy_temp_x0y0x1y1_to_mlx_x0y0x1y1(t_mlx *mlx, t_mlx *temp)
-{
-	mlx->x0 = temp->x0;
-	mlx->x1 = temp->x1;
-	mlx->y0 = temp->y0;
-	mlx->y1 = temp->y1;
 }
 
 void rotate_vertical_line(t_mlx *mlx, t_mlx *temp)
@@ -259,80 +137,6 @@ void rotate_horizontal_line(t_mlx *mlx, t_mlx *temp)
 	mlx->y1 = mlx->y1 + mlx->y_shift + (P_HEIGHT/4);
 }
 
-
-
-/*
-// Works and rotates map. Trying to add z value above.
-void rotate_vertical_line(t_mlx *mlx, t_mlx *temp)
-{
-	double degree_angle;
-
-	degree_angle = 150;	
-	if(mlx->y < temp->map_height)
-	{
-	mlx->z0 = mlx->int_data[mlx->y][mlx->x]; // Added z value
-	mlx->z1 = mlx->int_data[mlx->y + 1][mlx->x]; // Added z value
-	}
-	copy_mlx_x0y0x1y1_to_temp_x0y0x1y1(mlx, temp);
-	mlx->x1 = mlx->x1 + mlx->size;
-	subtract_x0y0_from_x1y1(mlx, temp);
-	rotation_matrix(mlx, temp, degree_angle);
-	add_rotated_x1y1_to_x0y0(mlx);
-
-
-	if(mlx->x == 0)
-	{
-		find_min_x(mlx, temp);
-		find_max_y(mlx, temp);
-	}
-}
-void rotate_horizontal_line(t_mlx *mlx, t_mlx *temp)
-{
-	double degree_angle;
-
-	degree_angle = 30;
-	if(mlx->x < temp->map_width)
-	{
-	mlx->z0 = mlx->int_data[mlx->y][mlx->x];   //Added z value 
-	mlx->z1 = mlx->int_data[mlx->y][mlx->x + 1]; //Added z value
-	}
-	copy_temp_x0y0x1y1_to_mlx_x0y0x1y1(mlx, temp);
-	mlx->x1 = mlx->x1 + mlx->size;
-	subtract_x0y0_from_x1y1(mlx, temp);
-	rotation_matrix(mlx, temp, degree_angle);
-	add_rotated_x1y1_to_x0y0(mlx);
-}
-*/
-/*
-** copy_temp_xy_to_mlx_x0y0x1y1 is used with find_min_x and find_max_y
-*/
-
-void copy_temp_xy_to_mlx_x0y0x1y1(t_mlx *mlx, t_mlx *temp)
-{
-	mlx->x0 = temp->x;
-	mlx->x1 = temp->x;
-	mlx->y0 = temp->y;
-	mlx->y1 = temp->y;
-}
-
-
-void find_min_x(t_mlx *mlx, t_mlx *temp)
-{
-	temp->x = 0;
-	if(mlx->x0 < mlx->x1)
-		temp->x = mlx->x0;
-	else if(mlx->x1 <= mlx->x0)
-		temp->x = mlx->x1;
-}
-
-void find_max_y(t_mlx *mlx, t_mlx *temp)
-{
-	temp->y = 0;
-	if(mlx->y0 > mlx->y1)
-		temp->y = mlx->y0;
-	else if(mlx->y1 >= mlx->y0)
-		temp->y = mlx->y1;
-}
 
 void rotate_x_axis(t_mlx *mlx, int *y, int *z, double x_axis)
 {
